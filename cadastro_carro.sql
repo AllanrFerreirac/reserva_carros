248d5e22587958bd59cef287f30f2df7ad995514
Allan Ferreira <89280714+AllanrFerreirac@users.noreply.github.com>
2021-11-29 14:13:01 -0300

    Add files via upload

--- cadastro_carro.sql
+++ cadastro_carro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29-Nov-2021 às 04:19
+-- Tempo de geração: 29-Nov-2021 às 18:09
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.9
 
@@ -29,22 +29,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cadastro_carro` (
   `id` int(11) NOT NULL,
-  `modelo` varchar(50) NOT NULL,
-  `placa` varchar(50) NOT NULL,
+  `modelo` varchar(20) NOT NULL,
+  `placa` varchar(20) NOT NULL,
   `ano` int(10) NOT NULL,
-  `estado` varchar(50) NOT NULL,
-  `manutencao` varchar(50) NOT NULL,
-  `data_inicio` varchar(10) NOT NULL,
-  `data_final` varchar(10) NOT NULL
+  `estado` varchar(20) NOT NULL,
+  `manutencao` varchar(20) NOT NULL,
+  `data_inicio` datetime NOT NULL,
+  `data_final` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `cadastro_carro`
---
-
-INSERT INTO `cadastro_carro` (`id`, `modelo`, `placa`, `ano`, `estado`, `manutencao`, `data_inicio`, `data_final`) VALUES
-(17, 'Citroen', 'AAA8E555', 2010, 'RIO', 'Disponivel', '', ''),
-(18, 'Ford', 'ABC8E848', 2008, 'Paraná', 'Disponivel', '', '');
 
 --
 -- Índices para tabelas despejadas
@@ -64,7 +56,7 @@ ALTER TABLE `cadastro_carro`
 -- AUTO_INCREMENT de tabela `cadastro_carro`
 --
 ALTER TABLE `cadastro_carro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
